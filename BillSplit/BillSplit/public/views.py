668298a7a15f91bb8e947d2c6561ac28bd60a4e5
freakazoidas <freakazoidas@gmail.com>
2023-01-24 04:04:30 +0200

    working on login

--- BillSplit/BillSplit/public/views.py
+++ BillSplit/BillSplit/public/views.py
@@ -74,22 +74,21 @@ def about():
 
 ##perdarymas i login page ######
 
-# @blueprint.route("/register/", methods=["GET", "POST"])
-# def register():
-#     """Register new user."""
-#     form = RegisterForm(request.form)
-#     if form.validate_on_submit():
-#         User.create(
-#             username=form.username.data,
-#             email=form.email.data,
-#             password=form.password.data,
-#             active=True,
-#         )
-#         flash("Thank you for registering. You can now log in.", "success")
-#         return redirect(url_for("public.home"))
-#     else:
-#         flash_errors(form)
-#     return render_template("public/register.html", form=form)
+@blueprint.route("/login/", methods=["GET", "POST"])
+def login():
+    """Home page."""
+    form = LoginForm(request.form)
+    current_app.logger.info("Hello from the home page!")
+    # Handle logging in
+    if request.method == "POST":
+        if form.validate_on_submit():
+            login_user(form.user)
+            flash("You are logged in.", "success")
+            redirect_url = request.args.get("next") or url_for("user.members")
+            return redirect(redirect_url)
+        else:
+            flash_errors(form)
+    return render_template("public/home.html", form=form)
 
 
 ##perdarymas i login page ######
